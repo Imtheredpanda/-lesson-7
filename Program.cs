6e8bd6582cadbb1966a93b648af6f18b0b937704
Alex <smith1alex@rambler.ru>
2024-07-03 22:07:57 +0400

    Урок 7 Промежуточная аттестация

--- Program.cs
+++ Program.cs
@@ -1,60 +1,60 @@
 ﻿// // ЗАДАЧА 1! Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
 
-//  void Nums(int m, int n)
-// {   
-//     if (n > m+1)
-//     {
-//         Console.WriteLine(m+1); 
-//         Nums(m+1,n);
-//     }
-//      if (m > n+1)
-//     {
-//         Console.WriteLine(n+1);
-//         Nums(m,n+1); 
-//     }
-// }
+ void Nums(int m, int n)
+{   
+    if (n > m+1)
+    {
+        Console.WriteLine(m+1); 
+        Nums(m+1,n);
+    }
+     if (m > n+1)
+    {
+        Console.WriteLine(n+1);
+        Nums(m,n+1); 
+    }
+}
 
-// Console.WriteLine("Введи число M:");
-// int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число M:");
+int m = Convert.ToInt32(Console.ReadLine());
 
-// Console.WriteLine("Введи число N:");
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число N:");
+int n = Convert.ToInt32(Console.ReadLine());
 
-// Nums(m,n);
+Nums(m,n);
 
 // // ЗАДАЧА 2! Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-// Console.Write("Введите число M: ");
-// int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
 
-// Console.Write("Введите число N: ");
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
 
-// AkkermanFunction(m,n);
+AkkermanFunction(m,n);
 
 
-// // вызов функции Аккермана
-// void AkkermanFunction(int m, int n)
-// {
-//     Console.Write(Akkerman(m, n)); 
-// }
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
 
-// // функция Аккермана
-// int Akkerman(int m, int n)
-// {
-//     if (m == 0)
-//     {
-//         return n + 1;
-//     }
-//     else if (n == 0 && m > 0)
-//     {
-//         return Akkerman(m - 1, 1);
-//     }
-//     else
-//     {
-//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
-//     }
-// }
+// функция Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
 
 
 
